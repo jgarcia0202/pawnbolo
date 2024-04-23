75abb56478a029652fc265fef398f4aaeb0a8897
Jose Garcia <jmgarcia0202@gmail.com>
2024-04-22 20:19:09 -0700

    login.js was added, able to go to signup.html and home.html

---
+++ js/login.js
@@ -0,0 +1,6 @@
+document.getElementById("signup").addEventListener("click", function() {
+    window.location.href = "./signup.html";
+})
+document.getElementById("login-button").addEventListener("click", function() {
+    window.location.href = "./home.html";
+})
